f3362a6f53ef1bf1d971620b047a4f3d5e393fc9
rylCodes <andresdaryl.94@gmail.com>
2024-01-18 23:25:15 +0800

    add header

--- daryl-andres/src/app/app.module.ts
+++ daryl-andres/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
+import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
+    HeaderComponent,
   ],
   imports: [
     BrowserModule,
