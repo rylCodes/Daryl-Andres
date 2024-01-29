1e8cd4e2262c70d9a20df27e421a34d75f2a8d5f
rylCodes <andresdaryl.94@gmail.com>
2024-01-29 16:57:24 +0800

    remove min view height in home section, add learn more in about section

--- daryl-andres/src/app/components/about/about.component.ts
+++ daryl-andres/src/app/components/about/about.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { faCircleChevronLeft, faCircleChevronRight } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-about',
@@ -6,4 +7,24 @@ import { Component } from '@angular/core';
   styleUrl: './about.component.scss'
 })
 export class AboutComponent {
+  faCircleChevronLeft = faCircleChevronLeft
+  faCircleChevronRight =faCircleChevronRight
+
+  isLearnMoreClicked: boolean = false;
+
+  imgSrcArr: string[] = [
+    '../../../assets/img/nf2.jpeg',
+    '../../../assets/img/nf1.jpeg',
+    '../../../assets/img/nf3.jpeg',
+    '../../../assets/img/ksk2.jpeg',
+    '../../../assets/img/ksk1.jpeg',
+    '../../../assets/img/lpe1.jpeg',
+    '../../../assets/img/lpe2.jpeg',
+  ]
+
+  imgSrc: string = this.imgSrcArr[0];
+
+  toggleLearnMore() {
+    this.isLearnMoreClicked = !this.isLearnMoreClicked;
+  }
 }
