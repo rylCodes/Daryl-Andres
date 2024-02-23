d2bfcaa551e22fa784517fa8ed9dc47e1843c69f
rylCodes <andresdaryl.94@gmail.com>
2024-02-23 17:40:12 +0800

    add new relevant certificates in resume section

--- daryl-andres/src/app/components/resume/resume.component.ts
+++ daryl-andres/src/app/components/resume/resume.component.ts
@@ -1,15 +1,6 @@
 import { Component, OnInit, ElementRef, Renderer2, ViewChild, HostListener } from '@angular/core';
 import { faImage, faDownload, faTimes, faCertificate, faFileArrowDown } from '@fortawesome/free-solid-svg-icons';
-
-interface Certificate {
-  html: string;
-  css: string;
-  js: string;
-  angularNetJs: string;
-  python: string;
-  pythonDataStructure: string;
-  responsiveWebDesign: string;
-}
+import { Certificate } from '../../interface/certificate';
 
 @Component({
   selector: 'app-resume',
@@ -49,6 +40,11 @@ export class ResumeComponent implements OnInit {
     python: '../../../assets/img/certificates/sololearn-python-introduction.png',
     pythonDataStructure: '../../../assets/img/certificates/sololearn-python-data-structure.png',
     responsiveWebDesign: '../../../assets/img/certificates/freecodecamp-responsive-web-design.JPG',
+    staticWebsite: '../../../assets/img/certificates/dict-lms-static-website.png',
+    basicJS: '../../../assets/img/certificates/dict-lms-javascript-basic.png',
+    flask: '../../../assets/img/certificates/dict-lms-flask-python.png',
+    pythonIntermediate: '../../../assets/img/certificates/dict-lms-python-intermediate.png',
+    pythonBeginner: '../../../assets/img/certificates/dict-lms-python-beginner.png',
   };
 
   certURL: Certificate = {
@@ -59,6 +55,11 @@ export class ResumeComponent implements OnInit {
     python: 'https://api2.sololearn.com/v2/certificates/CC-0PAX9KDD/image/png',
     pythonDataStructure: 'https://api2.sololearn.com/v2/certificates/CT-IOJ0VWCC/image/png',
     responsiveWebDesign: 'https://www.freecodecamp.org/certification/fccf25ac698-ea5b-417b-bdb6-59c9ad91fc03/responsive-web-design',
+    staticWebsite: 'https://courses.buri.io/view/user/certificate/2d224488-7d2c-44b9-8cc3-66d4814aa056/pdf',
+    basicJS: 'https://courses.buri.io/view/user/certificate/bd35c116-3e23-4e76-a65a-651e608660fb/pdf',
+    flask: 'https://courses.buri.io/view/user/certificate/44d1b505-fffa-4ad2-91a1-7024c9eeaf62/pdf',
+    pythonIntermediate: 'https://courses.buri.io/view/user/certificate/4959adde-9e13-4777-baf3-3cfc8be078a8/pdf',
+    pythonBeginner: 'https://courses.buri.io/view/user/certificate/34e7220b-7d25-4aa2-8e1b-9d68f15432f6/pdf',
   };
 
   certPlaceholder: Certificate = {
@@ -69,6 +70,11 @@ export class ResumeComponent implements OnInit {
     python: '../../../assets/img/20px/sololearn-python-introduction.png',
     pythonDataStructure: '../../../assets/img/20px/sololearn-python-data-structure.png',
     responsiveWebDesign: '../../../assets/img/20px/freecodecamp-responsive-web-design.jpg',
+    staticWebsite: '../../../assets/img/20px/dict-lms-static-website.png',
+    basicJS: '../../../assets/img/20px/dict-lms-javascript-basic.png',
+    flask: '../../../assets/img/20px/dict-lms-flask-python.png',
+    pythonIntermediate: '../../../assets/img/20px/dict-lms-python-intermediate.png',
+    pythonBeginner: '../../../assets/img/20px/dict-lms-python-beginner.png',
   };
 
   constructor(private el: ElementRef, private renderer: Renderer2) {}
---
+++ daryl-andres/src/app/interface/certificate.ts
@@ -0,0 +1,14 @@
+export interface Certificate {
+    html: string;
+    css: string;
+    js: string;
+    angularNetJs: string;
+    python: string;
+    pythonDataStructure: string;
+    responsiveWebDesign: string;
+    staticWebsite: string;
+    basicJS: string;
+    pythonBeginner: string;
+    pythonIntermediate: string;
+    flask: string;
+  }
