3afb4dc80d50ede153f83d3c04478e66c4872e07
rylcodes <andresdaryl.94@gmail.com>
2025-03-18 10:21:32 +0800

    fix: ensure to properly display the skeleton loader for projects

--- src/app/components/projects/projects.service.ts
+++ src/app/components/projects/projects.service.ts
@@ -1,6 +1,6 @@
 import { inject, Injectable, signal } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { map, catchError, Observable, of } from 'rxjs';
+import { map, catchError, Observable, of, tap } from 'rxjs';
 import { environment } from '@env/environment';
 import { Project } from '@data/models/project.model';
 import { projectsData } from '@data/personal-projects.data';
@@ -14,31 +14,49 @@ export class ProjectsService {
   isLoading = signal<boolean>(false);
 
   getProjects(): Observable<Project[]> {
+    this.isLoading.set(true);
     return this.http.get<{ results: Project[] }>(this.apiUrl).pipe(
+      tap(() => this.isLoading.set(false)),
       map((response) => {
         if (response.results.length === 0) {
           return projectsData;
         }
         return response.results;
       }),
-      catchError(() => of(projectsData)),
+      catchError((err) => {
+        console.error(err);
+        this.isLoading.set(false);
+        return of(projectsData);
+      }),
     );
   }
 
   getProject(id: number): Observable<Project> {
-    return this.http.get<Project>(`${this.apiUrl}/${id}`);
+    this.isLoading.set(true);
+    return this.http
+      .get<Project>(`${this.apiUrl}/${id}`)
+      .pipe(tap(() => this.isLoading.set(false)));
   }
 
   // For admin functionality
   createProject(project: Project): Observable<Project> {
-    return this.http.post<Project>(this.apiUrl, project);
+    this.isLoading.set(true);
+    return this.http
+      .post<Project>(this.apiUrl, project)
+      .pipe(tap(() => this.isLoading.set(false)));
   }
 
   updateProject(id: number, project: Project): Observable<Project> {
-    return this.http.put<Project>(`${this.apiUrl}/${id}`, project);
+    this.isLoading.set(true);
+    return this.http
+      .put<Project>(`${this.apiUrl}/${id}`, project)
+      .pipe(tap(() => this.isLoading.set(false)));
   }
 
   deleteProject(id: number): Observable<void> {
-    return this.http.delete<void>(`${this.apiUrl}/${id}`);
+    this.isLoading.set(true);
+    return this.http
+      .delete<void>(`${this.apiUrl}/${id}`)
+      .pipe(tap(() => this.isLoading.set(false)));
   }
 }
